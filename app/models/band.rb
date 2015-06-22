e5724add4b04a0cbd01642023ec8a7e58e5a1e35
Meg Svetz <megsvetz@gmail.com>
2015-06-22 16:12:43 -0600

    added a test to band model that returned what bands said

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,4 +3,7 @@ class Band < ActiveRecord::Base
   has_many :events
   has_many :venues, through: :events
 
+  def band_says
+    "#{self.name} is famous for saying: #{self.bio}."
+  end
 end
--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe Band, :type => :model do
 
   before(:each) do
-    @band1 = Band.create(name: 'Blink 182')
-    @band2 = Band.create(name: 'Green Day')
+    @band1 = Band.create(name: 'Blink 182', website: "http://www.facebook.com/blink182", bio: "We like to make Crappy Punk Rock")
+    @band2 = Band.create(name: 'Green Day', website: "http://www.greenday.com", bio: "We like to sing and dance")
   end
 
   it 'how many bands?' do
     expect(Band.all.count).to eq(2);
+  end
+
+  it 'returns what bands have said' do
+    expect(@band1.band_says).to eq("#{@band1.name} is famous for saying: #{@band1.bio}.")
   end
 
 end
