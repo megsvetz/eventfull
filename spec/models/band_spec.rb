29d26c496f514fe963d8ccfa164c6879131a6c51
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-06-22 15:55:26 -0600

    added a test to Band model

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Band, :type => :model do
 
+  before(:each) do
+    @band1 = Band.create(name: 'Blink 182')
+    @band2 = Band.create(name: 'Green Day')
+  end
+
+  it 'how many bands?' do
+    expect(Band.all.count).to eq(2);
+  end
+
 end
